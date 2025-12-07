0f0ea20b62679ca2e23365f8e1050c3c59db7b6c
HubertK <hubertkubs@gmail.com>
2025-12-07 22:08:28 +0100

    - Dodano mozliwosc wpisywania liczb z konsoli.

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Kalkulator {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 10;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe: ");
+        int x  = scanner.nextInt();
+        System.out.println("Podaj druga liczbe: ");
+        int y  = scanner.nextInt();
+
         int z = x + y;
-        System.out.println("Wynik dodawania to: " + z);
+
+        System.out.println("-----------------------------");
+        System.out.println("Wynik dzialania: " + x + " + " + y + " = " + z);
     }
 }
