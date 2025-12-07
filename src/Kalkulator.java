e002cde8adc67f9673b407ed22267276f846c823
HubertK <hubertkubs@gmail.com>
2025-12-07 22:02:34 +0100

    - Dodano dzialanie dodawania do klasy Kalkulator

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,5 +1,8 @@
 public class Kalkulator {
     public static void main(String[] args) {
-
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        System.out.println("Wynik dodawania to: " + z);
     }
 }
